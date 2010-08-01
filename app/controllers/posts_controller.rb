fe898d1fc6d17e0cb8c63f29753232e946f79666
Benjamin Lehn <ben@Benjamin-Lehns-MacBook-Pro.local>
2010-08-01 19:54:03 -0400

    completed chapter 11 all lessons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
 
   def create
     @post  = current_user.posts.build(params[:post])
@@ -13,5 +14,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    redirect_back_or root_path
   end
+
+  private
+
+    def authorized_user
+      @post = Post.find(params[:id])
+      redirect_to root_path unless current_user?(@post.user)
+    end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -63,4 +63,36 @@ describe PostsController do
       end
     end
   end
+
+  describe "DELETE 'destroy'" do
+
+    describe "for an unauthorized user" do
+
+      before(:each) do
+        @user = Factory(:user)
+        wrong_user = Factory(:user, :email => Factory.next(:email))
+        test_log_in(wrong_user)
+        @post = Factory(:post, :user => @user)
+      end
+
+      it "should deny access" do
+        delete :destroy, :id => @post
+        response.should redirect_to(root_path)
+      end
+    end
+
+    describe "for an authorized user" do
+
+      before(:each) do
+        @user = test_log_in(Factory(:user))
+        @post = Factory(:post, :user => @user)
+      end
+
+      it "should destroy the post" do
+        lambda do
+          delete :destroy, :id => @post
+        end.should change(Post, :count).by(-1)
+      end
+    end
+  end
 end
